dd43ec7a36a6d18d419fda5e3a716a985a1813da
Vittorio Fritelle <vitorjunior@poli.ufrj.br>
2021-07-15 17:56:26 -0300

    Adicionei as funcionalidades da aula

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,10 +1,23 @@
+/*
+Só é possível utilizar o import por 
+causa do webpack, que interpreta e 
+manda essa informação para o common.js, 
+de forma que os navegadores entendam essa sintaxe.
+*/
+import homeSectionOne from '../../components/sections/homeSectionOne'
+import homeSectionTwo from '../../components/sections/homeSectionTwo'
+import floatImage from '../../components/float/floatImage'
+
 let Home = {
   is_private: false,
 
   render: async () => {
       let view = `
           <div>
-            <h1>Home</div>
+            <!-- <h1>Início</div> -->
+            ${homeSectionOne}
+            ${homeSectionTwo}
+            ${floatImage}
           </div>
       `;
 
